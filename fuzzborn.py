84e7e45856b7b644515601e847efa5f4ebfafed8
Rohit Garg <cryptolemy@gmail.com>
2012-05-13 16:21:40 -0400

    No comment

--- fuzzborn.py
+++ fuzzborn.py
@@ -19,12 +19,15 @@ def append_to_queue(url):
 		crawlqueue.append(url)
 
 def add_to_params(url, type, param):
+	url = urlparse(url)
+	url = url.scheme + "://" + url.netloc + url.path
 	if url not in paramdict:
 		paramdict[url] = {}
 	tuple = paramdict[url]
 	if type not in tuple:
 		tuple[type] = []
-	tuple[type].append(param)
+	if param not in tuple[type]:
+		tuple[type].append(param)
 
 def analyze_inputs(url):
 	# Check the target location (deprecate?)
@@ -39,35 +42,42 @@ def analyze_inputs(url):
 				add_to_params(url, "get", param)
 		# Obtain the forms on this page
 		content = requests.get(url)
-		content = BeautifulSoup(content.text)
-		for form in content.find_all("form"):
-			# Check the post target location
-			target = convert_to_abs(url, form['action'])
-			if valid_target(target):
-				for input in form.find_all("input"):
-					add_to_params(target, "post", input['name'])
+		if content.headers['content-type'].startswith('text'):
+			content = BeautifulSoup(content.text)
+			for form in content.find_all("form"):
+				# Check the post target location
+				if 'action' in form.attrs:
+					target = convert_to_abs(url, form['action'])
+				else:
+					target = url
+				if valid_target(target):
+					for input in form.find_all("input"):
+						# Weed out dummy inputs
+						if 'name' in input.attrs:
+							add_to_params(target, "post", input['name'])
 					
 def children_of_page(url):
 	result = []
 	content = requests.get(url)
-	content = BeautifulSoup(content.text)
-	for link in content.find_all("a"):
-		if 'href' in link.attrs:
-			absurl = convert_to_abs(url, link['href'])
-			if valid_target(absurl):
-				result.append(absurl)
+	if content.headers['content-type'].startswith('text'):
+		content = BeautifulSoup(content.text)
+		for link in content.find_all("a"):
+			if 'href' in link.attrs:
+				absurl = convert_to_abs(url, link['href'])
+				if valid_target(absurl):
+					result.append(absurl)
 	return result
 				
 def crawl_recursively():
 	if crawlqueue:
-		url = crawlqueue.pop()
+		url = crawlqueue.pop(0)
+		append_to_visited(url)
 		analyze_inputs(url)
 		for child in children_of_page(url):
 			append_to_queue(child)
-		append_to_visited(url)
 		crawl_recursively()
 	else:
-		print visited
+		print paramdict
 		
 def convert_to_abs(parent, child):
 	parsedChild = urlparse(child)
