52f6f1ec44335f1bc5c1a6fb86283b74b092187d
Rohit Garg <cryptolemy@gmail.com>
2012-05-13 18:14:46 -0700

    Implemented password guessing and login, added more vectors.

--- fuzzer.py
+++ fuzzer.py
@@ -1,84 +1,209 @@
 #!/usr/bin/env python
-#
-# Demonstration of how to walk the links on a webpage with
-# requests and BeautifulSoup4.
-#
+import re
 import sys
-import config
+import time
+import copy
 import requests
-from time import time
+import settings
+
+from urlparse import urljoin, urlparse
 from bs4 import BeautifulSoup
-from collections import deque
 
-def get_children(url, count=0):
-	"""
-	Queue all the valid links referenced on this page.
+target = None
+session = None
+visited = []
+crawlqueue = []
+paramdict = {}
 
-	"""
+def append_to_visited(url):
+	visited.append(url)
 
-	children = []
-	if settings.debug:
-		print "Fetching: %s" % (root % url)
-	response = requests.get(root % url)
-	soup = BeautifulSoup(response.text)
+def append_to_queue(url):
+	if url not in visited and url not in crawlqueue:
+		crawlqueue.append(url)
 
-	# Dump response to a file
-	filename = "%s/%d.html" % (settings.output_dir, count)
-	count += 1
-	resp_dump = open(filename, "w")
-	try:
-		resp_dump.write(response.text)
-		print "Dumped to: %s" % filename
-	except UnicodeEncodeError:
-		pass
-	finally:
-		resp_dump.close()
+def add_to_params(url, type, param, value):
+	url = urlparse(url)
+	url = url.scheme + "://" + url.netloc + url.path
+	if url not in paramdict:
+		paramdict[url] = {}
+	tuple = paramdict[url]
+	if type not in tuple:
+		tuple[type] = {}
+	if param not in tuple[type]:
+		tuple[type][param] = value
 
-	for atag in soup.find_all('a'):
-		if check_prefixes(atag):
-			children.append(atag['href'])
+def analyze_inputs(url):
+	# Check the target location (deprecate?)
+	if valid_target(url):
+		# Obtain the GET inputs
+		query = urlparse(url).query
+		if query:
+			query = query.split('&')
+			# Get only the names, not the values
+			for tuple in query:
+				split = tuple.split('=')
+				param = split[0]
+				value = split[1]
+				add_to_params(url, "get", param, value)
+		# Obtain the forms on this page
+		content = session.get(url)
+		if content.headers['content-type'].startswith('text'):
+			content = BeautifulSoup(content.text)
+			for form in content.find_all("form"):
+				method = None
+				# Check the post method
+				if 'method' in form.attrs:
+					method = form['method'].lower()
+				else:
+					method = "get"
+				# Check the post target location
+				if 'action' in form.attrs:
+					target = urljoin(url, form['action'])
+				else:
+					target = url
+				if valid_target(target):
+					for input in form.find_all("input"):
+						# Weed out dummy inputs
+						if 'name' in input.attrs:
+							add_to_params(target, method, input['name'], "")
+					
+def children_of_page(url):
+	result = []
+	content = session.get(url)
+	if content.headers['content-type'].startswith('text'):
+		content = BeautifulSoup(content.text)
+		for link in content.find_all("a"):
+			if 'href' in link.attrs:
+				absurl = urljoin(url, link['href'])
+				if valid_target(absurl):
+					result.append(absurl)
+	return result
+				
+def crawl_recursively():
+	if crawlqueue:
+		url = crawlqueue.pop(0)
+		append_to_visited(url)
+		analyze_inputs(url)
+		if settings.page_discovery:
+			for child in children_of_page(url):
+				append_to_queue(child)
+			time.sleep(settings.wait_time)
+			crawl_recursively()
+
+def guess_pages():
+	if settings.page_guessing:
+		for page in settings.guess_pages:
+			url = urljoin("http://"+target, page)
+			result = session.get(url)
+			if result.status_code == 200:
+				append_to_queue(url)
+
+def guess_passwords():
+	if settings.pass_guessing:
+		response = None
+		for u,p in settings.guess_passwords.items():
+			payload = {}
+			data = settings.login_data
+			url = urljoin(data["proto"]+"://"+target, data["url"])
+			keys = data["data"].keys()
+			payload[keys[0]] = u
+			payload[keys[1]] = p
+			if data["rtype"] == "get":
+				response = requests.get(url, params=payload)
+			else:
+				response = requests.post(url, data=payload)
+			if data["success_text"] in response.text:
+				print "-"*80
+				print "Guessed login successful with %s:%s" % (u,p)
+
+def fuzz_all():
+	for url in paramdict:
+		fuzz_test(url)
+
+def fuzz_test(url):
+	if settings.fuzz_complete:
+		# Iterate over each test
+		for name, test in settings.fuzz_tests.items():
+			# Iterate over types
+			for type, params in paramdict[url].items():
+				# Iterate over each parameter
+				for param in params:
+					# Iterate over each vector
+					for vector in test["vector"]:
+						fail = False
+						result = None
+						payload = copy.copy(params)
+						payload[param] = vector
+						if type == "get":
+							result = session.get(url, params=payload)
+						else:
+							result = session.post(url, data=payload)
+						if 'fail_results' in test:
+							for string in test["fail_results"]:
+								if string in result.text:
+									fail = True
+							if fail:
+								print "-"*80
+								print test["fail_message"] % (type.upper(), url, param)
+
+def print_attack_surface():
+	print "=" * 80
+	print "Attack Surface:"
+	print "=" * 80
+	for url, types in paramdict.items():
+		print ">"*80
+		print url
+		for type, params in types.items():
+			print type.upper()
+			for param, value in params.items():
+				print "%s (Default: %s)" % (param,value)
+				
+def valid_target(url):
+	location = urlparse(url).netloc
+	if location == target:
+		return True
+	else:
+		return False
 		
-	return soup, children
+def set_target(url):
+	global target
+	# Obtain the domain
+	target = urlparse(url).netloc
 
-def check_prefixes(atag):
-	"""
-	Don't leave the domain, don't mess with FTP, don't try and follow mailto: links.
-
-	"""
-
-	bad_prefixes = ["http", "ftp", "mailto"]
-	if atag.has_key('href'):
-		for prefix in bad_prefixes:
-			if str(atag['href']).startswith(prefix):
-				return False
-		if not atag['href'] in queue and not atag['href'] in visited:
-			print "Found link: " + str(atag['href'])
-			return True
-	return False
+def login():
+	if settings.attempt_login:
+		response = None
+		login_data = settings.login_data
+		url = urljoin(login_data["proto"]+"://"+target, login_data["url"])
+		if login_data["rtype"] == "get":
+			response = session.get(url, login_data["data"])
+		else:
+			response = session.post(url, login_data["data"])
+		if login_data["success_text"] in response.text:
+			print "Login successful."
+		
 
 if __name__ == "__main__":
 	if(len(sys.argv) > 1):
-		print sys.argv[1]
-	"""
-	queue = settings.default_actions
-	visited = []
-	formFields = settings.default_parameters
-	root = settings.base_url
-	count = 0
-
-	while len(queue) > 0:
-		count += 1
-		url = queue.pop()
-		visited.append(url)
-		soup, children = get_children(url, count)
-		for child in children:
-			if child not in visited:
-				queue.append(child)
-		for form in soup.find_all("form"):
-			for field in form.find_all("input"):
-				if field not in formFields:
-					formFields.append(field['name'])
-	
-	print formFields
-	print visited
-	"""
+		# Create a session
+		session = requests.session()
+		# Set the target
+		set_target(sys.argv[1])
+		# Add the given page to the queue
+		append_to_queue(sys.argv[1])
+		# Attempt to login
+		login()
+		# Guess passwords
+		guess_passwords()
+		# Guess pages
+		guess_pages()
+		# Crawl
+		crawl_recursively()
+		# Fuzz
+		fuzz_all()
+		# Print human readable attack surface
+		print_attack_surface()
+	else:
+		print "Usage: %s [target]" % (sys.argv[0])
+		print "Don't forget the trailing slash!"
